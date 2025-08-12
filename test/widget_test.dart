9ff2197362bfda0ac62ec54ec13b8c673ebc0811
whevether <whevether@outlook.com>
2025-08-12 08:04:14 +0800

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_template/services/user_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_bloc_template/main.dart';
@@ -14,8 +13,7 @@ import 'package:flutter_bloc_template/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    var userBloc = UserBloc(UserState(user: null, loginResult: null));
-    await tester.pumpWidget( MyApp(userBloc: userBloc,));
+    await tester.pumpWidget( const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
