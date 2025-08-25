76b2105284594f9d9f12aaee7a8fac33db0e9372
keep_wan <whevether@outlook.com>
2025-08-25 10:13:47 +0800

    Update list_event.dart

--- lib/app/base/list/list_event.dart
+++ lib/app/base/list/list_event.dart
@@ -1,4 +1,3 @@
-import 'package:easy_refresh/easy_refresh.dart';
 import 'package:flutter_bloc_template/app/app_constant.dart';
 import 'package:flutter_bloc_template/app/base/bloc/base_event.dart';
 import 'package:flutter_bloc_template/app/base/list/list_bloc.dart';
@@ -42,15 +41,16 @@ class InitEvent<T> extends ListEvent<T> {
     bloc.loadDone();
     // 如果返回的数据不存在则返回一个空列表
     List<T> list = <T>[];
+    // 如果当前页数等于总页数，表示没有更多数据
+    if (bloc.pageNum == bean[AppConstant.totalPageKey]) {
+      return InitialState(list);
+    }
     var result = bean[AppConstant.resultKey];
     for (var item in result) {
       list.add(item as T);
     }
 
-    // 如果当前页数等于总页数，表示没有更多数据
-    if (bloc.pageNum == bean[AppConstant.totalPageKey]) {
-      bloc.controller.finishLoad(IndicatorResult.noMore);
-    }
+    
     // 更新当前状态的数据
     return InitialState(list);
   }
@@ -87,14 +87,15 @@ class RefreshEvent<T> extends ListEvent<T> {
     }
     // 如果返回的数据不存在则返回一个空列表
     List<T> list = <T>[];
+    // 如果当前页数等于总页数，表示没有更多数据
+    if (bloc.pageNum == bean[AppConstant.totalPageKey]) {
+      return InitialState(list);
+    }
     var result = bean[AppConstant.resultKey];
     for (var item in result) {
       list.add(item as T);
     }
-    // 如果当前页数等于总页数，表示没有更多数据
-    if (bloc.pageNum == bean[AppConstant.totalPageKey]) {
-      bloc.controller.finishRefresh(IndicatorResult.noMore);
-    }
+    
     // 更新当前状态的数据
     return InitialState(list);
   }
@@ -124,29 +125,28 @@ class LoadMoreEvent<T> extends ListEvent<T> {
     }
     // 检查状态码是否正确
     if (bean == null) {
-      bloc.controller.finishLoad(IndicatorResult.fail);
       bloc.loadMoreError();
       return currentState;
     }
     // 如果返回的数据不存在则返回一个空列表
     List<T> list = <T>[];
+    // 如果当前页数等于总页数，表示没有更多数据
+    if (bloc.pageNum == bean[AppConstant.totalPageKey]) {
+       return currentState;
+    }
     var result = bean[AppConstant.resultKey];
     for (var item in result) {
       list.add(item as T);
     }
     // 如果没有数据，直接返回
     if (list.isEmpty) {
-      bloc.controller.finishLoad(IndicatorResult.noMore);
       return currentState;
     }
     // 更新当前状态的数据
     currentState.data.addAll(list);
-    // 如果当前页数等于总页数，表示没有更多数据
-    if (bloc.pageNum == bean[AppConstant.totalPageKey]) {
-      bloc.controller.finishLoad(IndicatorResult.noMore);
-    }
+    
     // 返回更新后的状态
-    return InitialState(currentState.data);
+     return currentState;
   }
 }
 
