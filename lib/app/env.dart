8f23750d8c4a7b642bc80f2470ac97f84600e5d4
wanwenfeng <wanwenfeng@qq.com>
2026-01-09 14:51:47 +0800

    update

--- lib/app/env.dart
+++ lib/app/env.dart
@@ -4,9 +4,9 @@ class Env {
   //api基础地址
   static const String apiBaseUrl = String.fromEnvironment('API_BASE_URL');
   //http端口
-  static int httpPort = String.fromEnvironment('HTTP_PORT') as int;
+  static const String httpPort = String.fromEnvironment('HTTP_PORT');
   //socket端口
-  static int socketPort = String.fromEnvironment('SOCKET_PORT') as int;
+  static const String socketPort = String.fromEnvironment('SOCKET_PORT');
   //是否开启日志
   static bool enableLog = String.fromEnvironment('ENABLE_LOG') as bool;
 }
